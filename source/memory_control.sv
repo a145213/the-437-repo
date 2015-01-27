cea24adc6910e4553621d390a132e496a02d78a2
Allen Chien <achien@purdue.edu>
2015-01-27 18:49:16 -0500

    memory controller

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -22,4 +22,37 @@ module memory_control (
   // number of cpus for cc
   parameter CPUS = 2;
 
+assign ccif.iload = ccif.ramload;
+assign ccif.dload = ccif.ramload;
+assign ccif.ramstore = ccif.dstore;
+
+always_comb begin
+  ccif.iwait = 0;
+  ccif.dwait = 0;
+  casez(ccif.ramstate)
+    FREE: begin
+      ccif.iwait = 0;
+      ccif.dwait = 0;
+    end
+    BUSY: begin
+      ccif.iwait = 1;
+      ccif.dwait = 1;
+    end
+    ACCESS: begin
+      if (ccif.dWEN[0] || ccif.dREN[0]) begin
+        ccif.iwait = 1;
+        ccif.dwait = 0;
+      end
+      else if (ccif.iREN[0]) begin
+        ccif.iwait = 0;
+        ccif.dwait = 1;
+      end
+    end
+    ERROR: begin
+    ccif.iwait = 1;
+    ccif.dwait = 1;
+    end
+  endcase
+end
+
 endmodule
